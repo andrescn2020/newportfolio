f8fb8a745d1cfdd050cf2393ca68f269990d0ba4
Andres <andrescn_2010@hotmail.com>
2022-12-09 18:58:46 -0300

    Argentina a semis

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,8 @@
 import { useForm } from 'react-hook-form';
 import emailjs from "@emailjs/browser";
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import "./Contact.css";
 
 const MySwal = withReactContent(Swal);
 
@@ -31,34 +32,61 @@ const Contact = () => {
 
   return (
     <form className='app__form' onSubmit={handleSubmit(sendEmail)}>
-      <h1 className='app__form-fitle'>Contacto</h1>
+      <h1 className='app__form-title'>Contacto</h1>
       <div className='app__form-container'>
         <div className='app__form-text'>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem repellat recusandae quia obcaecati dolorum omnis ad quis. Sit, perferendis incidunt. Sed cumque neque officiis vero ipsa laborum, adipisci vel possimus!</p>
+          <p>Pongamosnos en contacto!</p>
         </div>
-        <div className='separator-line'></div>
-        <div>
+        <div className='app__form-inputs'>
           <div className="app-form-group-name">
-            <input className="app-form-control" name='nombre' type="text" placeholder="NOMBRE..." {...register("nombre", {
+            <input className="app-form-control" name='nombre' type="text" placeholder="Nombre" {...register("nombre", {
               required: true,
               maxLength: 16
             })} />
             {errors.nombre ? <p style={{ color: "red" }}>Este campo es obligatorio debe tener un maximo de 16 caracteres</p> : null}
           </div>
           <div className="app-form-group-mail">
-            <input className="app-form-control" name='mail' type="text" placeholder="MAIL..." {...register("email", {
+            <input className="app-form-control" name='mail' type="text" placeholder="Email" {...register("email", {
+              pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
+            })} />
+            {errors.email ? <p style={{ color: "red" }}>Este campo no cumple con el formato requerido</p> : null}
+          </div>
+        </div>
+        <div className="app-form-group-message">
+          <textarea className="app-form-control-message" name='mensaje' type="text" placeholder="Mensaje" {...register("descripcion")} />
+        </div>
+        <div className="app-form-group-buttons">
+          <input type="submit" value="Enviar" className="app-form-button" />
+        </div>
+      </div>
+      {/* <h1 className='app__form-title'>Contacto</h1>
+      <div className='app__form-container'>
+        <div className='app__form-text'>
+          <p>Si tienes alguna consulta sobre mi que no se encuentre por aqui o quieres charlar conmigo para acordar entrevistas o un proyecto a desarrollar acorde a mi perfil no dudes en escribirme!</p>
+        </div>
+        <div className='separator-line'></div>
+        <div className='app__form-inputs'>
+          <div className="app-form-group-name">
+            <input className="app-form-control" name='nombre' type="text" placeholder="Nombre" {...register("nombre", {
+              required: true,
+              maxLength: 16
+            })} />
+            {errors.nombre ? <p style={{ color: "red" }}>Este campo es obligatorio debe tener un maximo de 16 caracteres</p> : null}
+          </div>
+          <div className="app-form-group-mail">
+            <input className="app-form-control" name='mail' type="text" placeholder="Email" {...register("email", {
               pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
             })} />
             {errors.email ? <p style={{ color: "red" }}>Este campo no cumple con el formato requerido</p> : null}
           </div>
           <div className="app-form-group-message">
-            <input className="app-form-control" name='mensaje' type="text" placeholder="MENSAJE..." {...register("descripcion")} />
+            <input className="app-form-control" name='mensaje' type="text" placeholder="Mensaje" {...register("descripcion")} />
           </div>
           <div className="app-form-group-buttons">
             <input type="submit" value="Enviar" className="app-form-button" />
           </div>
         </div>
-      </div>
+      </div> */}
     </form>
   )
 };
