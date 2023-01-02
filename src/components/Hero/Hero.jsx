69a330567ed9e726012ee8b5d24730aee6dc13ec
Andres <andrescn_2010@hotmail.com>
2023-01-01 23:05:00 -0300

    Efectos de botones

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,13 +12,33 @@ const Hero = () => {
         <img className="app__hero-img" src={HeroText} alt="hero logo" />
         <h2 className="app__hero-text">FULL STACK DEVELOPER</h2>
         <div className="app__hero-link-container">
-          {language === "en"
+          {/* {language === "en"
             ? <a className="app__hero-link" href="https://www.dropbox.com/s/dv77w8btllu9l3j/Resume-AndresCapano-FullStackDeveloper.pdf?dl=0" target="_blank" rel="noopener noreferrer">
               <button>Resume</button>
             </a>
             : <a className="app__hero-link" href="https://www.dropbox.com/s/vrqvoa499ws3u3v/CV-AndresCapano-FullStackDeveloper.pdf?dl=0" target="_blank" rel="noopener noreferrer">
               <button>Ver CV</button>
-            </a>}
+            </a>} */}
+          {language === "en"
+            ? <a href="https://www.dropbox.com/s/dv77w8btllu9l3j/Resume-AndresCapano-FullStackDeveloper.pdf?dl=0" target="_blank" rel="noopener noreferrer">
+            <button>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Resume
+            </button>
+            </a>
+            : <a href="https://www.dropbox.com/s/vrqvoa499ws3u3v/CV-AndresCapano-FullStackDeveloper.pdf?dl=0" target="_blank" rel="noopener noreferrer">
+            <button>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Ver CV
+            </button>
+            </a>
+            }
         </div>
       </div>
     </header>
